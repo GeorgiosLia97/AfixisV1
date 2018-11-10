311e87e171e0286fa55dd51862d71c19da13b8e0
Georgios Liapis <gewrgios.liapis@gmail.com>
2018-11-10 14:29:31 +0200

    v5

--- app/src/main/java/com/example/liapi_000/afixisv1/Maps.java
+++ app/src/main/java/com/example/liapi_000/afixisv1/Maps.java
@@ -11,7 +11,10 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
@@ -20,6 +23,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.IOException;
 import java.util.List;
@@ -33,9 +37,13 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
     private static final int REQUEST_LOCATION_PERMISSION = 1;
     Marker marker;
     LocationListener locationListener;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cfd);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -51,9 +59,14 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
                     REQUEST_LOCATION_PERMISSION);
         }
         locationListener = new LocationListener() {
+
+
             @Override
             public void onLocationChanged(Location location) {
-                double latitude = location.getLatitude();
+                Log.e("drinks", "onMapReady: run ok");
+                //double latitude = 40.59392;
+              double  latitude = location.getLatitude();
+                //double longitude = 22.95184;
                 double longitude = location.getLongitude();
                 //get the location name from latitude and longitude
                 Geocoder geocoder = new Geocoder(getApplicationContext());
@@ -66,11 +79,11 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
                     if (marker != null) {
                         marker.remove();
                         marker = mMap.addMarker(new MarkerOptions().position(latLng).title(result));
-                        mMap.setMaxZoomPreference(20);
+                        mMap.setMaxZoomPreference(10);
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
                     } else {
                         marker = mMap.addMarker(new MarkerOptions().position(latLng).title(result));
-                        mMap.setMaxZoomPreference(20);
+                        mMap.setMaxZoomPreference(10);
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 21.0f));
                     }
 
@@ -114,21 +127,117 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
 
-       // Location lastKnownLocation = locationManager.getLastKnownLocation(locationListener.toString());
+        String provider = LocationManager.NETWORK_PROVIDER;
 
-       // locationListener.onLocationChanged(lastKnownLocation);
+        if( locationManager.isProviderEnabled(provider)){
+
+            Log.e( "drinks", "onMapReady: passive ok");
+        } else {
+
+            provider = LocationManager.GPS_PROVIDER;
+
+            if( locationManager.isProviderEnabled(provider)){
+
+                Log.e("drinks2", "onMapReady: gps ok");
+            } else {
+                provider = LocationManager.PASSIVE_PROVIDER;
+
+                if( locationManager.isProviderEnabled(provider)){
+                    Log.e("drinks3", "onMapReady: network ok" );
+                }
+            }
+
+
+        }
+
+        /*if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            @Override
+            public void onRequestPermissionsResult(int requestCode,
+            String permissions[], int[] grantResults) {
+                switch (requestCode) {
+                    case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                        // If request is cancelled, the result arrays are empty.
+                        if (grantResults.length > 0
+                                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                            // permission was granted, yay! Do the
+                            // contacts-related task you need to do.
+                        } else {
+                            // permission denied, boo! Disable the
+                            // functionality that depends on this permission.
+                        }
+                        return;
+                    }
+
+                    // other 'case' lines to check for other
+                    // permissions this app might request.
+                }
+            }
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            return;
+        }*/
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+      //Location la = locationManager.getLastKnownLocation(provider);
+       //locationManager.requestLocationUpdates(provider, 1000, 10f, locationListener);
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+                        if(location != null) {
+
+                            locationListener.onLocationChanged(location);
+                        } else {
+                            Log.e("drinks5", "onMapReady: null loc" );
+                        }
+
+                    }
+                });
+
+    //Location la =
+
+                // Location lastKnownLocation = locationManager.getLastKnownLocation(locationListener.toString());
+
+        //locationListener.onLocationChanged(la);
+
+        /*double a = 0;
+        double b = 0;
+
+        a = loc.getLatitude();
+        b = loc.getLongitude();
+
+        LatLng here = new LatLng(a , b); */
 
         //  Add a marker in Sydney and move the camera
-        LatLng here = new LatLng(40.595392, 22.951841);
-       mMap.addMarker(new MarkerOptions().position(here).title("Marker in Sydney"));
+       /* LatLng here = new LatLng(40.595392, 22.951841);
+       mMap.addMarker(new MarkerOptions().position(here).title("Marker here"));
        mMap.moveCamera(CameraUpdateFactory.newLatLng(here));
-       mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(here, 15));
+       mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(here, 15));*/
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         locationManager.removeUpdates(locationListener);
+
     }
 
 
