fd6e5f494bd051ceef704da156bd93dacf168c5f
Georgios Liapis <gewrgios.liapis@gmail.com>
2018-11-10 10:53:15 +0200

    v5

--- app/src/main/java/com/example/liapi_000/afixisv1/CFD.java
+++ app/src/main/java/com/example/liapi_000/afixisv1/CFD.java
@@ -1,42 +1,121 @@
 package com.example.liapi_000.afixisv1;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+/*
 
-public class CFD extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+public abstract class CFD extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener,  GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
+    private static final MediaPlayer LocationRequest = ;
+    private GoogleApiClient mGoogleApiClient;
     private GoogleMap mMap;
+    private int ft = 0;
+    Location location;
+    public static final String TAG = CFD.class.getSimpleName();
+    private LocationRequest mLocationRequest;
+
+
+    public CFD(GoogleApiClient mGoogleApiClient) {
+        this.mGoogleApiClient = mGoogleApiClient;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mm);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+       // super.onCreate(savedInstanceState);
+       // setContentView(R.layout.mm);
 
         /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);*/
-
+//}
+/*
+    @Override
+    public void onConnected(Bundle bundle) {
+       // Log.i(TAG, "Location services connected.");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+        else {
+            handleNewLocation(location);
+        }
     }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+       // Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    private void handleNewLocation(Location location) {
+        //Log.d(TAG, location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!");
+        mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
 
     /**
      * Manipulates the map once available.
@@ -46,45 +125,99 @@ public class CFD extends FragmentActivity implements OnMapReadyCallback, GoogleM
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
-     */
+     *//*
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             Criteria criteria = new Criteria();
 
-            //Location myLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-           /* double latitude = myLocation.getLatitude();
+            Location myLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+            double latitude = myLocation.getLatitude();
             double longtitude = myLocation.getLongitude();
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longtitude),1));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longtitude), 1));
             CameraPosition cameraPosition = new CameraPosition.Builder()
                     .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
                     .build();                   // Creates a CameraPosition from the builder
-            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));   */
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             mMap.setMyLocationEnabled(true);
 
-            location = mMap.getMyLocation();
 
-            if (location != null) {
-                LatLng myLocation = new LatLng(location.getLatitude(),
-                        location.getLongitude());
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
 
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom((myLocation), 1));
-            }
-           // return;
+            // location = mMap.getMyLocation();
+
+            //  if (location != null) {
+            //     myLocation = new LatLng(location.getLatitude(),
+            //          location.getLongitude());
+
+            //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom((myLocation), 1));
+            //}
+            // return;
+
         }
-
-
-
     }
 
+            @Override
+            public void onMyLocationClick(@NonNull Location location) {
+                Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public boolean onMyLocationButtonClick() {
+                Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+                // Return false so that we don't consume the event and the default behavior still occurs
+                // (the camera animates to the user's current position).
+                return false;
+            }
+
+*/
 
 
 
+
+/*
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            // mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+            //       .getMap();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+
+
+                mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+
+                    @Override
+                    public void onMyLocationChange(Location arg0) {
+                        // TODO Auto-generated method stub
+
+                        mMap.addMarker(new MarkerOptions().position(new LatLng(arg0.getLatitude(), arg0.getLongitude())).title("It's Me!"));
+                    }
+                });
+
+            }
+        }
+    }
+
+*/
   /*  private void updateLocationUI() {
 
     }
@@ -93,12 +226,18 @@ public class CFD extends FragmentActivity implements OnMapReadyCallback, GoogleM
 
     }
 */
-
+/*
     public void onClickDrinks(View view){
-        setContentView(R.layout.activity_cfd);
+       /* if( ft == 0) {
+            onMapReady(mMap);
+    ft =1;
+        }*/
 
-    }
+        //setUpMapIfNeeded();
+        //    setContentView(R.layout.activity_cfd);
 
+       // }
+/*
     public void onClickFood(View view){
         setContentView(R.layout.activity_cfd);
 
@@ -117,7 +256,7 @@ public class CFD extends FragmentActivity implements OnMapReadyCallback, GoogleM
         System.exit(0);
     }
 
-    @Override
+ /*   @Override
     public boolean onMyLocationButtonClick() {
         return false;
     }
@@ -126,10 +265,43 @@ public class CFD extends FragmentActivity implements OnMapReadyCallback, GoogleM
     public void onMyLocationClick(@NonNull Location location) {
 
     }
+*/
 
 
 
+//}
 
+public class CFD extends AppCompatActivity {
+
+    int ft = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (ft == 0) {
+            super.onCreate(savedInstanceState);
+            ft = 1;
+        }
+        setContentView(R.layout.mm);
+
+    }
+
+    public void onClickDrinks(View view) {
+
+        showMyLocation(view);
+        //setContentView(R.layout.activity_cfd);
+    }
+
+    public void showMyLocation(View view) {
+        startActivity(new Intent(getApplicationContext(),Maps.class));
+    }
+
+    public void onClickBack(View view){
+        setContentView(R.layout.mm);
+    }
+
+    public void onClickExit(View view){
+        finish();
+        System.exit(0);
+    }
 }
-
-
---
+++ app/src/main/java/com/example/liapi_000/afixisv1/LocationRequest.java
@@ -0,0 +1,9 @@
+//package com.example.liapi_000.afixisv1;
+
+import android.os.Parcelable;
+
+import com.google.android.gms.common.internal.safeparcel.AbstractSafeParcelable;
+
+//class abstract LocationRequest extends AbstractSafeParcelable
+  //      implements Parcelable {
+//}
